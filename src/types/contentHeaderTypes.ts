503da8814219d4b00743e374af05b4c47e855b71
Sminp <0102262@gmail.com>
2026-01-15 12:37:20 +0900

    feat: press와 view 관련 type 추가
    - view type 생성
    
    - category, press, list press로 확장성 있는 type 설계

---
+++ src/types/contentHeaderTypes.ts
@@ -0,0 +1,1 @@
+export type viewType = 'grid' | 'list';
---
+++ src/types/pressTypes.ts
@@ -0,0 +1,29 @@
+export type category =
+  | '종합/경제'
+  | '방송/통신'
+  | 'IT'
+  | '영자지'
+  | '스포츠/연예'
+  | '매거진/전문지'
+  | '지역';
+
+export interface press {
+  press: string;
+  logo: string;
+  darkLogo: string;
+}
+
+interface relatedArticle {
+  title: string;
+  link: string;
+}
+
+export interface listPress extends press {
+  category: category;
+  totalPage: string;
+  time: string;
+  mainTitle: string;
+  mainLink: string;
+  mainImg: string;
+  relatedArticles: relatedArticle[];
+}
