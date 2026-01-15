a21d146f2227b41d23324ee27dc2d012369bb6ce
Sminp <0102262@gmail.com>
2026-01-15 10:46:53 +0900

    feat: 뉴스 헤드라인에 호버링 시 일시정지 기능 추가

--- src/components/rolling/RollingItem.tsx
+++ src/components/rolling/RollingItem.tsx
@@ -9,6 +9,15 @@ const headlineLength = NEWS_HEADLINE_DATAS.length;
 
 const RollingItem = ({ itemNumber, currentTarget }: RollingItemProps) => {
   const [currentPressIndex, setCurrentPressIndex] = useState(itemNumber);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
 
   const rolling = () => {
     setCurrentPressIndex((idx) => (idx + 2) % headlineLength);
@@ -17,12 +26,19 @@ const RollingItem = ({ itemNumber, currentTarget }: RollingItemProps) => {
 
   useEffect(() => {
     if (itemNumber !== currentTarget) return;
+
+    if (isHovering) return;
+
     onRolling();
   }, [currentTarget, itemNumber]);
 
   return (
     <div className='flex justify-start items-start gap-2 flex-1'>
-      <div className='flex justify-start items-start flex-grow relative gap-4 p-4 bg-[#f5f7f9] border border-[#d2dae0] w-full'>
+      <div
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        className='flex justify-start items-start flex-grow relative gap-4 p-4 bg-[#f5f7f9] border border-[#d2dae0] w-full'
+      >
         <p className='flex-shrink-0 text-sm font-bold text-left text-[#14212b]'>
           {NEWS_HEADLINE_DATAS[currentPressIndex].press}
         </p>
