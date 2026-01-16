e86fc590f0f13d9cb8c54ab6c2603a4a8af8c20b
Sminp <0102262@gmail.com>
2026-01-16 12:31:24 +0900

    feat: 재사용 가능한 Button 컴포넌트를 클릭했을 때 이벤트를 발생시키기 위한 handleClick 함수를 사용하여 onClick에 연결 Co-authored-by: 윤종근 <hi2242@users.noreply.github.com> Co-authored-by: 강유진 <using2@users.noreply.github.com>

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,11 +1,13 @@
 interface ButtonProps {
   text: string;
   className?: string;
+  handleClick?: () => void;
 }
 
-const Button = ({ text, className }: ButtonProps) => {
+const Button = ({ text, className, handleClick }: ButtonProps) => {
   return (
     <button
+      onClick={handleClick}
       className={`text-[#879298] border border-[#D2DAE0] px-1 py-1 text-xs ${className}`}
     >
       {text}
